6dc2d1c3c510c217d5bfdfe73409aa52ee5eef82
greensTech67 <greensseleniumtask@gmail.com>
2019-12-20 10:21:57 +0530

    FirstCode

---
+++ src/org/selenium/LaunchBrowser.java
@@ -0,0 +1,49 @@
+package org.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+	
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\admin\\eclipse-workspace\\sELENIUM\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.facebook.com/");
+		
+		//To find the locator
+		
+		WebElement mail = driver.findElement(By.xpath("//input[@type='email']"));
+		mail.sendKeys("saranya");
+		
+		String att1 = mail.getAttribute("class");
+		System.out.println(att1);
+		
+		
+		//To fill password
+		
+		WebElement pwd = driver.findElement(By.xpath("(//input[@type='password'])[1]"));
+		pwd.sendKeys("345678");
+		
+		
+		
+		
+		//To click button
+		
+		WebElement btn = driver.findElement(By.xpath("//label[@id='loginbutton']"));
+		btn.click();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
